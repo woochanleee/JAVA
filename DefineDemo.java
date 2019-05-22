c0a1b4e90e0ee069b691a467f47a7e62f576ff93
woochanleee <030219woo@naver.com>
2019-05-22 17:11:44 +0900

    lecture_1

--- DefineDemo.java
+++ DefineDemo.java
@@ -1,6 +1,6 @@
 public class DefineDemo {
 
     public static void main(String[] args) {
-        String[] classgroup = { "최진혁", "최유빈", "한이람", "이고잉" };
+        String[] classGroup = { "최진혁", "최유빈", "한이람", "이고잉" };
     }
 }
