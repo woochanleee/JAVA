19e9e8cf04f3928a19e110958060d2adbc688350
woochanleee <030219woo@naver.com>
2019-05-22 23:52:47 +0900

    lecture_15

--- Scanner3Demo.java
+++ Scanner3Demo.java
@@ -1,12 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Scanner2Demo {
+public class Scanner3Demo {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            System.out.println(sc.nextInt() * 1000);
-        }
-        sc.close();
+       try  {
+           File file = new File("out.txt");
+           Scanner sc = new Scanner(file);
+           while (sc.hasNextInt()) {
+               System.out.println(sc.nextInt() * 1000);
+           }
+           sc.close();
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       }
     }
 }
