c0f2ee581ef748a40d663d0e2199fa59a01fe676
woochanleee <030219woo@naver.com>
2019-05-22 20:12:21 +0900

    lecture_8

---
+++ MethodDemo4.java
@@ -0,0 +1,14 @@
+public class MethodDemo3 {
+
+    public static void numbering(int limit) {
+        int i = 0;
+        while (i < limit) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        numbering(5 );
+    }
+}
