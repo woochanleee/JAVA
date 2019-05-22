c6704381a2ae5ecc06549e739a8c1e68a07a33f7
woochanleee <030219woo@naver.com>
2019-05-22 23:52:27 +0900

    lecture-14

--- Scanner2Demo.java
+++ Scanner2Demo.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class ScannerDemo {
+public class Scanner2Demo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        System.out.println(i * 1000);
+        while (sc.hasNextInt()) {
+            System.out.println(sc.nextInt() * 1000);
+        }
         sc.close();
     }
 }
