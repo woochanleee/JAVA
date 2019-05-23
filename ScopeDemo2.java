8fff27620348785cfe8b30fbea6d2da48aac1175
woochanleee <030219woo@naver.com>
2019-05-23 18:18:58 +0900

    lecture_20

--- ScopeDemo2.java
+++ ScopeDemo2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ScopeDemo2 {
+    static int i;
+
+    static void a() {
+        i = 0;
+    }
+
+    public static void main(String[] args) {
+        for (i = 0; i < 5; i++) {
+            a();
+            System.out. println(i);
+        }
 }
+// 무한반복
